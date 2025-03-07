f91b2991d43991fd2645b602100a51d4a8b90aff
Vicen Dominguez <vicente.dominguez@flywire.com>
2025-03-07 12:37:30 +0100

    fixes codecompanion

--- private_dot_config/nvim/lua/plugins/codecompanion.lua
+++ private_dot_config/nvim/lua/plugins/codecompanion.lua
@@ -1,32 +1,33 @@
 return {
  {
    "olimorris/codecompanion.nvim",
-   config = true,
    dependencies = {
      "nvim-lua/plenary.nvim",
      "nvim-treesitter/nvim-treesitter",
    },
- },
-
- require("codecompanion").setup({
-  adapters = {
-     qwencoder = function()
-      return require("codecompanion.adapters").extend("ollama", {
-        name = "qwencoder", -- Give this adapter a different name to differentiate it from the default ollama adapter
-        schema = {
-          model = {
-            default = "qwen2.5-coder:14b",
-          },
-          num_ctx = {
-            default = 16384,
-          },
-          num_predict = {
-            default = -1,
-          },
+   opts = {
+      strategies = {
+        chat = { adapter = 'ollama' },
+        inline = { adapter = 'ollama' },
+        adapters = {
+           qwencoder = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              name = "qwencoder", -- Give this adapter a different name to differentiate it from the default ollama adapter
+              schema = {
+                model = {
+                  default = "qwen2.5-coder:7b",
+                },
+                num_ctx = {
+                  default = 16384,
+                },
+                num_predict = {
+                  default = -1,
+                },
+              },
+            })
+          end,
         },
-      })
-    end,
-  },
- })
-
+      }
+   } 
+ },
 }
