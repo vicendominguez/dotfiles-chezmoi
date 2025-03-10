d8c6207cdd468912a31f01325a9840701508dae2
Vicen Dominguez <vicente.dominguez@flywire.com>
2025-03-11 00:06:47 +0100

    [main] Update Treesitter plugins to include gotmpl, comment, and json.

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -7,18 +7,20 @@ return {
      require("nvim-treesitter.install").update({ with_sync = true })("lua")
      require("nvim-treesitter.install").update({ with_sync = true })("vim")
      require("nvim-treesitter.install").update({ with_sync = true })("vimdoc")
-     require("nvim-treesitter.install").update({ with_sync = true })("markdown")
+     require("nvim-treesitter.install").update({ with_sync = true })("gotmpl")
+     require("nvim-treesitter.install").update({ with_sync = true })("comment")
+     require("nvim-treesitter.install").update({ with_sync = true })("json")
    end,
    config = function()
      require("nvim-treesitter.configs").setup({
-       ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go" }, -- Minimal install
+       ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go" , "comment", "gotmpl" }, -- Minimal install
        sync_install = false,
        auto_install = true,
        highlight = {
          enable = true,
          additional_vim_regex_highlighting = false,
        }, 
-       indent = { enable = true },    -- Habilita indentación basada en treesitter
+       indent = { enable = true },    
      })
    end,
   },
