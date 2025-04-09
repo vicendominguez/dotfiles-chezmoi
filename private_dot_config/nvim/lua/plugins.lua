9cd082f6a2f49cbc867ac2a843f719df9f71ac3a
vicen <vicen@pluto.local>
2025-04-09 14:18:04 +0200

    testing lazygit instead of fugitive

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -4,7 +4,6 @@ return {
   { "folke/lazy.nvim", version = "*" },
   { "nvim-lualine/lualine.nvim", config = function() require("lualine").setup() end },
   { "ibhagwan/fzf-lua", config = function() require("fzf-lua").setup() end },
-  { "tpope/vim-fugitive" },
   { "numToStr/Comment.nvim", config = function() require("Comment").setup() end },
   { "nvim-tree/nvim-tree.lua", config = function() require("nvim-tree").setup() end },
   { "lukas-reineke/indent-blankline.nvim", config = function() require("ibl").setup() end },
