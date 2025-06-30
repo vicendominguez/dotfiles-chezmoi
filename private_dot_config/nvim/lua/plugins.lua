74679c199c11b6c9e5d4f776800e1af4bcb87d80
quasar <quasar@localhost.localdomain>
2025-06-30 23:47:57 +0200

    test lualine: adding absolute path

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -2,7 +2,14 @@
 -- complex config Plugins in plugins/
 return {
   { "folke/lazy.nvim", version = "*" },
-  { "nvim-lualine/lualine.nvim", config = function() require("lualine").setup({options = { theme = "auto"}}) end },
+  { "nvim-lualine/lualine.nvim", config = function()
+    require("lualine").setup({
+      options = { theme = "auto" },
+      sections = {
+        lualine_c = { { 'filename', path = 1 } }, -- Display absolute path for the current file
+      }
+    })
+  end },
   { "ibhagwan/fzf-lua", config = function() require("fzf-lua").setup() end },
   { "numToStr/Comment.nvim", config = function() require("Comment").setup() end },
   { "nvim-tree/nvim-tree.lua", config = function() require("nvim-tree").setup() end },
