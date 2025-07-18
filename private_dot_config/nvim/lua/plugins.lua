a97a1f6aabf51bad34704adbf84c23bfb1912332
Vicen Dominguez <vicente.dominguez@flywire.com>
2025-07-18 14:16:26 +0200

    going to mini plugin system

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -11,9 +11,18 @@ return {
     })
   end },
   { "ibhagwan/fzf-lua", config = function() require("fzf-lua").setup() end },
-  { "numToStr/Comment.nvim", config = function() require("Comment").setup() end },
   { "nvim-tree/nvim-tree.lua", config = function() require("nvim-tree").setup() end },
   { "lukas-reineke/indent-blankline.nvim", config = function() require("ibl").setup() end },
-  { 'echasnovski/mini.pairs', config = function () require("mini.pairs").setup() end, version = false },
+  { "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+      require("mini.pairs").setup()
+      require("mini.comment").setup()
+      require("mini.surround").setup()
+      require("mini.trailspace").setup()
+      require("mini.splitjoin").setup()
+      require("mini.hipatterns").setup()
+    end,
+  },
 }
 
